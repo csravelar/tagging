5cbc2ad1c017e935404cd615fb5626e5d205720a
Richard Avelar <richard.avelar@asurion.com>
2021-02-24 10:56:51 -0600

    test

--- tag_project.py
+++ tag_project.py
@@ -4,6 +4,12 @@ import sys
 
 import requests
 
+os.environ["checkmarx_config_path"] = f"{os.getcwd()}/config.ini"
+
+from CheckmarxPythonSDK.CxODataApiSDK import get_all_projects_id_name
+from CheckmarxPythonSDK.CxRestAPISDK import ProjectsAPI
+from CheckmarxPythonSDK.CxRestAPISDK.sast.projects.dto.customFields import CxCustomField
+
 
 def check_valid_platform_tag(platform_tag):
     """Check the user supplied platform tag against cloudgov tags to ensure validity"""
@@ -29,11 +35,6 @@ def get_project_info(project_name):
 
 
 def main():
-    print("\n\n-----------------------\nWe start here\n---------------------\n\n")
-    print(f"{os.environ.get('checkmarx_config_path')}")
-    from CheckmarxPythonSDK.CxODataApiSDK import get_all_projects_id_name
-    from CheckmarxPythonSDK.CxRestAPISDK import ProjectsAPI
-    from CheckmarxPythonSDK.CxRestAPISDK.sast.projects.dto.customFields import CxCustomField
     project_name = os.environ.get("PROJECT_NAME")
     platform_tag = os.environ.get("PLATFORM_TAG")
 
