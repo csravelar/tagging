2c0f4bf8d2fac066337adedc571f08a9eb6f38c1
Richard Avelar <richard.avelar@asurion.com>
2021-02-24 00:15:37 -0600

    test

--- create_checkmarx_ini_config.py
+++ create_checkmarx_ini_config.py
@@ -6,12 +6,7 @@ url = os.environ.get("CHECKMARX_SERVER")
 username = os.environ.get("CHECKMARX_USERNAME")
 password = os.environ.get("CHECKMARX_PASSWORD")
 
-print("HERE ARE THE DEETS")
-print(url)
-print(username)
-print(password)
-
-loader = FileSystemLoader(".")
+loader = FileSystemLoader("/")
 jinja_env = Environment(loader=loader)
 template = jinja_env.get_template("config_template.txt")
 
