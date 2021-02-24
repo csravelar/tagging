8653753ae7b43e86f78c744864f8ffd0d516d80f
Richard Avelar <richard.avelar@asurion.com>
2021-02-23 23:53:59 -0600

    test

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,6 @@ FROM python:3.8.7-slim-buster
 COPY . /
 
 RUN apt-get update && \
-    pip install -r requirements.txt && \
-    mkdir /root/.Checkmarx && \
-    python3 create_checkmarx_ini_config.py 
+    pip install -r requirements.txt
 
 ENTRYPOINT ["/entrypoint.sh"]
--- create_checkmarx_ini_config.py
+++ create_checkmarx_ini_config.py
@@ -15,5 +15,7 @@ loader = FileSystemLoader(".")
 jinja_env = Environment(loader=loader)
 template = jinja_env.get_template("config_template.txt")
 
+os.mkdir("~/.Checkmarx")
+
 with open("~/.Checkmarx/config.ini", "w") as config:
     config.write(template.render(base_url=url, username=username, password=password))
