c1e1bf5a3d234f18efe7a5f6dde5df683e3d61e0
Richard Avelar <richard.avelar@asurion.com>
2021-02-24 01:04:45 -0600

    test

--- create_checkmarx_ini_config.py
+++ create_checkmarx_ini_config.py
@@ -10,7 +10,7 @@ loader = FileSystemLoader("/")
 jinja_env = Environment(loader=loader)
 template = jinja_env.get_template("config_template.txt")
 
-os.mkdir("/.Checkmarx")
+os.mkdir(".Checkmarx")
 
-with open("/.Checkmarx/config.ini", "w") as config:
+with open(".Checkmarx/config.ini", "w") as config:
     config.write(template.render(base_url=url, username=username, password=password))
