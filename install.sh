c53c5f7a0901ebb3b4670fa724f15bd0b79cdf86
Richard Avelar <richard.avelar@asurion.com>
2021-03-01 17:25:12 -0600

    test

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ sudo -E yum update
 sudo -E yum install curl git
 sudo -E touch /usr/local/bin/lol.txt
 sudo -E curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo -E python get-pip.py
+sudo -E python3 get-pip.py
 sudo -E git clone https://github.com/csravelar/tagging.git /usr/local/bin/tagging
-sudo -E pip install -r /usr/loca/bin/tagging/requirements.txt
+sudo -E pip3 install -r /usr/loca/bin/tagging/requirements.txt
