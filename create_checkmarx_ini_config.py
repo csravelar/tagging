fd904e9148fa52922e7401da6f187f8f88f2a199
Richard Avelar <richard.avelar@asurion.com>
2021-03-03 15:16:09 -0600

    test

--- create_checkmarx_ini_config.py
+++ create_checkmarx_ini_config.py
@@ -1,12 +1,19 @@
+"""
+Run this program to create your config.ini file that checkmarxpythonsdk
+needs to use to authenticate with the api
+"""
 import os
 import pathlib
 
 from jinja2 import Environment, FileSystemLoader
 
+
+def main():
 base_url = os.environ["CHECKMARX_SERVER"]
 username = os.environ["CHECKMARX_USERNAME"]
 password = os.environ["CHECKMARX_PASSWORD"]
 
+# Tell jinja where in the filesytem we are going to read a file from
 cwd_path = str(pathlib.Path(__file__).parent.absolute())
 loader = FileSystemLoader(cwd_path)
 jinja_env = Environment(loader=loader)
@@ -16,3 +23,7 @@ with open("config.ini", "w") as config:
     config.write(
         template.render(base_url=base_url, username=username, password=password)
     )
+
+
+if __name__ == "__main__":
+    main()
